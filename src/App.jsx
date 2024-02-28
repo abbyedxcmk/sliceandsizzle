7086278a9b4feba78b3a1034ea00f28617c02e11
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-02-28 20:57:52 +0000

    Add Contact for components folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Supersellers from './components/Supersellers';
 import Menu from './components/Menu';
 import Feedbacks from './components/Feedbacks';
+import Contact from './components/Contact';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Menu />
       <Supersellers />
       <Feedbacks />
+      <Contact />
     </>
   )
 }
